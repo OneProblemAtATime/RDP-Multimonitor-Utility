cf82bac64b0daa53744e3b4ed4ed5ae16c6fffaf
OneProblemAtATime <oneproblematatime09@gmail.com>
2024-05-07 14:09:35 -0500

    Notes about what to make.

---
+++ main.py
@@ -0,0 +1,5 @@
+# Need to make a tool that is able to search for rdp files
+#   |then it should detect what type of monitors are to be
+#   |used by the RDP session. After this, it should map
+#   |setting screens to RDP screens and allow the user to
+#   |choose the screens to pass to the RDP session.
